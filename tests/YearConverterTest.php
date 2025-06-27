353831b5ad37fcee96bb8ca504c73066bca78591
nissan <hexiliang04@gmail.com>
2025-06-28 01:34:26 +0900

    refactor: optimize YearConverterTest by reusing YearConverter instance

--- tests/YearConverterTest.php
+++ tests/YearConverterTest.php
@@ -6,17 +6,25 @@ use PHPUnit\Framework\Attributes\Test;
 
 class YearConverterTest extends TestCase
 {
+    private YearConverter $yearConverter;
+    
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->yearConverter = new YearConverter();
+    }
+
     #[Test]
     public function 西暦から和暦への変換が正しく行われること()
     {
-        $yearConverter = new YearConverter();
-        $this->assertEquals($yearConverter->seirekiToWareki(2023), '令和5年');
+        $this->assertEquals('令和5年', $this->yearConverter->seirekiToWareki(2023));
     }
 
     #[Test]
     public function 和暦から西暦への変換が正しく行われること()
     {
         $yearConverter = new YearConverter();
-        $this->assertEquals($yearConverter->warekiToSeireki('令和5年'), 2023);
+        $this->assertEquals(2023, $this->yearConverter->warekiToSeireki('令和5年'));
     }
 }
